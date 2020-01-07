310d3d3b3134e92ea892f5a733f38557fd53a3e3
Prakriti Dhir <p2dhir@uwaterloo.ca>
2020-01-07 10:22:54 -0500

    Add files via upload

---
+++ arrays/fibonacciNumber.java
@@ -0,0 +1,18 @@
+
+public class fibonacciNumber {
+	public int fib(int N) {
+        if(N == 0 || N == 1) {
+            return N;
+        }
+        
+        int[] fib = new int[N + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for(int i = 2; i <= N; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        
+        return fib[N];
+    }
+}
